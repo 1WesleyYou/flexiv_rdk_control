8080d3bb61791fe99871d15d12a867839aaf6d69
1Wesleyyou <yuchenyou76@gmail.com>
2024-04-07 20:35:24 +0800

    feat: python 通过本地文件读取数据集，然后再进行发包，通过simulate_fetch.cpp 进行测试

--- data_generator.py
+++ data_generator.py
@@ -2,7 +2,11 @@ import subprocess
 import time
 import json
 import random
+import os
 
+# 先确保编译文件
+os.system(
+    'home/whoami/.local/share/JetBrains/Toolbox/apps/CLion/ch-0/233.13135.93/bin/cmake/linux/x64/bin/cmake --build /home/whoami/Desktop/flexiv_control/cmake-build-debug --target demo --config Release -j 4')
 # 替换为你的C++程序的路径
 cpp_program_path = 'cmake-build-debug/demo'
 
@@ -19,7 +23,7 @@ def update_data():
     global current_positions, current_velocities
     for i in range(7):
         position_change = random.uniform(-0.01, 0.01)  # 小幅度调整
-        velocity_change = random.uniform(-0.005, 0.005)  # 小幅度调整
+        velocity_change = random.uniform(-0.0005, 0.0005)  # 小幅度调整
         current_positions[i] += position_change
         current_velocities[i] += velocity_change
 
@@ -40,6 +44,6 @@ try:
         data = generate_data()
         p.stdin.write(data.encode())
         p.stdin.flush()  # 确保数据被发送
-        time.sleep(0.1)  # 以10Hz频率发送
+        time.sleep(0.01)  # 以10Hz频率发送
 except KeyboardInterrupt:
     p.terminate()  # 用户中断脚本时终止C++程序
--- test_control.cpp
+++ test_control.cpp
@@ -7,23 +7,40 @@
 #include <flexiv/Robot.hpp>
 #include <flexiv/Utility.hpp>
 
+#include "lib/nlohmann/json.hpp"
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <vector>
 
 const int MAX_INSTRUCTIONS = 30;
 
-void loadInstruction(std::string &str) {
-  std::fstream iFile;
-  iFile.open("des");
-  if (!iFile) {
-    std::cerr << "Cant open file!" << std::endl;
+// 定义两个向量相加
+std::vector<double> vector_add(const std::vector<double> &v1,
+                               const std::vector<double> &v2) {
+  if (v1.size() != v2.size()) {
+    throw std::invalid_argument("Vectors must be of the same size");
   }
-  return;
+
+  std::vector<double> result(v1.size());
+  for (size_t i = 0; i < v1.size(); ++i) {
+    result[i] = v1[i] + v2[i];
+  }
+
+  return result;
+}
+
+void vector_deg2rad(std::vector<double> &v1) {
+  for (double &i : v1) {
+    i = i / 180 * 3.1415926535;
+  }
 }
 
 int main() {
+  // 使用nlohmann/json库的命名空间
+  using json = nlohmann::json;
+
   flexiv::Log log;
   std::string robotIP = "192.168.2.100";
   std::string localIP = "192.168.2.105"; // todo: 记得修改这里的 ip 地址
@@ -31,11 +48,14 @@ int main() {
   flexiv::Gripper gripper(robot);
   std::string instructions[MAX_INSTRUCTIONS];
   std::fstream iFile;
-  iFile.open("../des");
-  if (!iFile) {
-    std::cout << "cant open file!" << std::endl;
-    return 0;
-  }
+
+  std::vector<double> default_pos = {0.0, -40.0, 0.0, 90.0, 0.0, 40.0, 0.0};
+  vector_deg2rad(default_pos);
+  std::vector<double> default_vel = {0.2, 0.2, 0.2,
+                                     0.2, 0.2, 0.2}; // 默认速度 0.2 m/s
+  std::vector<double> default_acc = {0, 0, 0, 0, 0, 0, 0};
+  std::vector<double> real_pos = {0, 0, 0, 0, 0, 0, 0};
+  std::vector<double> real_vel = {0, 0, 0, 0, 0, 0, 0};
 
   log.info("You Yuchen's Control Demo:");
   // 清除错误指令（如果可行）
@@ -55,45 +75,56 @@ int main() {
   // Enable the robot, make sure the E-stop is released before enabling
   log.info("Enabling robot ...");
   robot.enable();
-  robot.setMode(flexiv::Mode::NRT_PRIMITIVE_EXECUTION);
+  robot.setMode(
+      flexiv::Mode::NRT_PRIMITIVE_EXECUTION); // 初始化需要执行 home 函数
   gripper.move(0.09, 0.1, 20);
   robot.executePrimitive("Home()");
   // Wait for reached target
   while (flexiv::utility::parsePtStates(robot.getPrimitiveStates(),
                                         "reachedTarget") != "1") {
     std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-  std::string cmds;
-  while (getline(iFile, cmds)) {
+  } // 完成初始化，这里达到 home 位置
 
-    log.info("Going to destination [" + cmds + "]");
+  robot.setMode(
+      flexiv::Mode::RT_JOINT_POSITION); // 根据输入使用实时 joint 速度位置控制
 
-    robot.executePrimitive("MoveL(target=" + cmds + ")");
+  // 读取数据直到EOF
+  for (std::string line; std::getline(std::cin, line);) {
+    try {
+      // 解析JSON数据
+      auto j = json::parse(line);
 
-    while (flexiv::utility::parsePtStates(robot.getPrimitiveStates(),
-                                          "reachedTarget") != "1") {
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+      // 检查键是否存在以及是否为预期的类型
+      if (j.contains("joint_positions") && j["joint_positions"].is_array() &&
+          j.contains("joint_velocities") && j["joint_velocities"].is_array()) {
+
+        // 从JSON对象中提取向量
+        std::vector<double> joint_positions =
+            j["joint_positions"].get<std::vector<double>>();
+        std::vector<double> joint_velocities =
+            j["joint_velocities"].get<std::vector<double>>();
+
+        std::cout << "\n";
+
+        std::cout << "pos ";
+        vector_deg2rad(joint_positions);
+        real_pos = vector_add(default_pos, joint_positions);
+        //        real_vel = vector_add(real_vel, joint_velocities);
+        for (const auto &vel : real_pos) {
+          std::cout << vel << " ";
+        }
+        std::cout << std::endl;
+        for (const auto &vel : joint_velocities) {
+          std::cout << vel << " ";
+        }
+        std::cout << std::endl;
+        robot.streamJointPosition(real_pos, joint_velocities, default_acc);
+      }
+    } catch (json::parse_error &e) {
+      std::cerr << "Parse error: " << e.what() << std::endl;
+      // 处理错误或跳过无效的数据包
     }
   }
-  gripper.move(0.03, 0.9, 20);
-  std::this_thread::sleep_for(std::chrono::seconds(2));
-
-  robot.executePrimitive(
-      "MoveL(target=0.4833 -0.3194 0.11 180 0 180 WORLD WORLD_ORIGIN)");
-  while (flexiv::utility::parsePtStates(robot.getPrimitiveStates(),
-                                        "reachedTarget") != "1") {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-  robot.executePrimitive(
-      "MoveL(target=0.4833 -0.3194 0.071 180 0 180 WORLD WORLD_ORIGIN)");
-
-  gripper.move(0.09, 0.09, 20);
-  robot.executePrimitive("Home()");
-  while (flexiv::utility::parsePtStates(robot.getPrimitiveStates(),
-                                        "reachedTarget") != "1") {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
   robot.stop();
-  //    robot.executePrimitive()
   return 0;
 }
