ec1e3d95ffcb329411e06b426b0e81779a14a5a2
whoami <yuchenyou76@gmail.com>
2024-05-29 21:58:19 +0800

    feat: find suitable moving data order_moderate.txt

--- app/data_generator.py
+++ app/data_generator.py
@@ -6,7 +6,7 @@ import json
 import os
 
 # 替换为你的C++程序的路径
-cpp_program_path = '../cmake-build-debug/base/test_every_joint'
+cpp_program_path = "../cmake-build-debug/base/test_every_joint"
 
 # 启动C++程序并创建一个到其标准输入的管道
 p = subprocess.Popen(cpp_program_path, stdin=subprocess.PIPE)
@@ -14,54 +14,68 @@ p = subprocess.Popen(cpp_program_path, stdin=subprocess.PIPE)
 time_cnt = 0
 
 # 文件路径
-file_path = '../simulate_datasets/offset_order.txt'
-time_span = 40  # 假设这里是 100 ms 时间延迟，具体的需要按收包实际时间差考虑
+file_path = "../simulate_datasets/order_moderate.txt"
+time_span = 60  # 假设这里是 100 ms 时间延迟，具体的需要按收包实际时间差考虑
 total_time = 250
-last_data = [0, -40 * np.pi / 180, 0, 90 * np.pi / 180, 0, 40 / 180 * np.pi, 0]
+last_data = [0, -40.0 * np.pi / 180.0, 0, 90 * np.pi / 180, 0, 40 / 180 * np.pi, 0]
 data_a4_last = []
 data_a4_angle = []
 is_gripper_closed = False
 
 try:
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         for line in file:
-            line = line.replace('None', 'null')
+            line = line.replace("None", "null")
             try:
                 data = json.loads(line)  # 解析JSON
-                if data['joint_positions'][3]:
-                    data_a4_angle.append(data['joint_positions'][3])
-                
-                is_gripper_closed = bool(data['joint_positions'][0])
+                if data["joint_positions"][3]:
+                    data_a4_angle.append(data["joint_positions"][2])
+
+                is_gripper_closed = bool(data["joint_positions"][0])
 
                 diff_data = []
                 # 计算两个变量的差值，用于预测下一帧的走向
-                if data['joint_positions'][0]:
+                if data["joint_positions"][0]:
                     for j in range(7):
-                        diff_data.append((data['joint_positions'][j] - last_data[j]) / time_span)
-                    last_data = data['joint_positions']
+                        diff_data.append(
+                            (data["joint_positions"][j] - last_data[j]) / time_span
+                        )
+                    last_data = data["joint_positions"]
 
                 for i in range(time_span):
                     local_data = [0, 0, 0, 0, 0, 0, 0]
-                    if data['joint_positions'][0]:
+                    if data["joint_positions"][0]:
                         for j in range(7):
-                            local_data[j] = data['joint_positions'][j]  # todo: 这里可能是硬拷贝的问题，明天有空查一下
+                            local_data[j] = data["joint_positions"][
+                                j
+                            ]  # todo: 这里可能是硬拷贝的问题，明天有空查一下
                             local_data[j] = local_data[j] + diff_data[j] * i
                             # if j == 3:
                             # data_a4_last.append(local_data[3])
                     # 确保每一行都是有效的JSON格式
-                    if data['joint_positions'][0]:
-                        local_datas = {'joint_positions': local_data, 'joint_velocities': [0, 0, 0, 0, 0, 0, 0],
-                                       'joint_efforts': is_gripper_closed}
+                    if data["joint_positions"][0]:
+                        local_datas = {
+                            "joint_positions": local_data,
+                            "joint_velocities": [0, 0, 0, 0, 0, 0, 0],
+                            "joint_efforts": is_gripper_closed,
+                        }
                     else:
-                        local_datas = {'joint_positions': last_data, 'joint_velocities': [0, 0, 0, 0, 0, 0, 0],
-                                       'joint_efforts': is_gripper_closed}
-                    json_string = json.dumps(local_datas)  # 将解析后的JSON重新编码为字符串
-                    data_a4_last.append(local_data[0] * 180 / np.pi)
-                    p.stdin.write(json_string.encode() + b'\n')  # 发送JSON字符串给C++程序
+                        local_datas = {
+                            "joint_positions": last_data,
+                            "joint_velocities": [0, 0, 0, 0, 0, 0, 0],
+                            "joint_efforts": is_gripper_closed,
+                        }
+                    json_string = json.dumps(
+                        local_datas
+                    )  # 将解析后的JSON重新编码为字符串
+                    data_a4_last.append(local_data[2] * 180 / np.pi)
+                    p.stdin.write(
+                        json_string.encode() + b"\n"
+                    )  # 发送JSON字符串给C++程序
                     p.stdin.flush()  # 确保数据被发送
                     time.sleep(0.001)  # 以1000Hz的频率发送数据
                     time_cnt = time_cnt + 0.001
-                    #print(time_cnt)
+                    # print(time_cnt)
 
                 # data_a4_last.append(last_data[3])
                 if time_cnt >= total_time:
@@ -69,7 +83,7 @@ try:
 
             except json.JSONDecodeError as e:
                 print(f"JSON解析错误: {e}")
-        t = np.linspace(0, total_time, int(total_time / 0.001) + 100)
+        t = np.linspace(0, total_time, int(total_time / 0.001))
         t2 = np.linspace(0, 20, 20 * 10)
 
         # plt.scatter(t2, data_a4_angle)
